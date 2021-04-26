09fa0e085623dac1df9bb2f9ba9b14c4a489df81
icwb <83106848+icwb@users.noreply.github.com>
2021-04-26 19:07:43 +0700

    update

---
+++ _next/static/pBCROz_kuMytdJI4Qh2_F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-dff69d747b4cbd383a4d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-eb3c772407df2a9232d7.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
